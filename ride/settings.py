8a019e0b597f230181dd2130d9e0e0ac3a19fa0d
Eric Parker <eparker71@gmail.com>
2020-10-20 23:11:56 -0400

    added default value for ENV get. this should default to dev if ENV is not set

--- ride/settings.py
+++ ride/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from ride.secure import secure_settings
 
-ENV = os.environ['ENV']
+ENV = os.environ.get('ENV', "dev")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
