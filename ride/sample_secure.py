78aa74e11cfe6dff4b5c8881c8ca8c4813fc50bd
Eric Parker <eparker71@gmail.com>
2020-10-20 23:19:14 -0400

    updated instructions for sample_secure.py

--- ride/sample_secure.py
+++ ride/sample_secure.py
@@ -1,4 +1,5 @@
 # Edit this file, update the following with appropriate values
+# Then rename this file to secure.py 
 secure_settings = {
     "DJANGO_KEY" : "<your django secret key>",
     "API_KEY" : "<mbta api key>",
