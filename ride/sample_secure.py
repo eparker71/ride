11a394264a673fdf28af7ade8805ff422758b13e
Eric Parker <eparker71@gmail.com>
2020-10-20 23:18:06 -0400

    added sample_secure.py in the proper location with proper dict structure

---
+++ ride/sample_secure.py
@@ -0,0 +1,7 @@
+# Edit this file, update the following with appropriate values
+secure_settings = {
+    "DJANGO_KEY" : "<your django secret key>",
+    "API_KEY" : "<mbta api key>",
+    "MAP_KEY" : "<google map api key>",
+}
+
